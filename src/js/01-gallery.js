87739e39aa17939de6e5f86d251a884b5d7282e8
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-06-19 21:21:26 +0300

    1-2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(`.gallery`);
+galleryRef.innerHTML = renderGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: `alt`, captionDelay: 250,});
+
+
+
+function renderGallery(params) {
+    return params.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+    }).join("");
+}
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")));
+
+
+    player.on('timeupdate', throttle((event) => {
+        localStorage.setItem("videoplayer-current-time", event.seconds);
+}, 1000));
