75af2c6d172609083c35379015cce560a41fcc7a
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-06-20 20:31:58 +0300

    final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+
+const formRef = document.querySelector(`.feedback-form`);
+const inputRef = document.querySelector(`.js-input`);
+const messageRef = document.querySelector(`.js-message`);
+
+let saveObject = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+if(saveObject) {
+    const {email = "", message = ""} = saveObject;
+    inputRef.value = email;
+    messageRef.value = message;
+} else (saveObject = {})
+
+formRef.addEventListener(`input`, throttle((event) => {
+    saveObject[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(saveObject));
+}, 500))
+
+formRef.addEventListener(`submit`, e => {
+    e.preventDefault();
+    console.log({email: inputRef.value, message: messageRef.value});
+    inputRef.value = "";
+    messageRef.value = "";
+    localStorage.clear();
+})
