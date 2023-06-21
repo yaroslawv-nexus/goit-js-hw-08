1bb4341324ee88d104dfa69e561deda38926b3d3
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-06-21 11:12:29 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,13 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const player = new Player('vimeo-player');
-player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")));
+
+const localCheck = localStorage.getItem("videoplayer-current-time");
+
+if(localCheck) {
+    player.setCurrentTime(JSON.parse(localCheck));
+}
+
 
     player.on('timeupdate', throttle((event) => {
         localStorage.setItem("videoplayer-current-time", event.seconds);
